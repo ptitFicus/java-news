f884c6c93cffaed56f32fd6e4939c39b9e2b20b4
Benjamin CAVY <benjamin.cavy@maif.fr>
2022-05-19 01:13:35 +0200

    fix: tests name

--- src/test/java/fr/maif/demo/part11/time/TimeStuffTest.java
+++ src/test/java/fr/maif/demo/part11/time/TimeStuffTest.java
@@ -9,14 +9,14 @@ import java.time.*;
 
 public class TimeStuffTest {
     @Test
-    public void isThis2023ShouldReturnTrueToday() {
+    public void isThis2050ShouldReturnTrueToday() {
         final TimeStuff timeStuff = new TimeStuff(InstantSource.system());
 
         assertFalse(timeStuff.isThis2050());
     }
 
     @Test
-    public void isThis2023ShouldReturnTrueIfYearIs2050() {
+    public void isThis2050ShouldReturnTrueIfYearIs2050() {
         final LocalDateTime time = LocalDateTime.of(2050, 1, 1, 0, 0);
         final Instant instant = time.atZone(ZoneId.systemDefault()).toInstant();
         final TimeStuff timeStuff = new TimeStuff(InstantSource.fixed(instant));
